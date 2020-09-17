1f7bd962f9598d91f03f3c97b64263e5dc5d8604
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-09-17 12:41:59 -0700

    SAVEPOINT

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const uuid = require('uuid');
 
 const { NODE_ENV } = require('./config');
+const mockData = require('./mockData');
 
 const app = express();
+app.use(express.json());
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
@@ -14,8 +17,35 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+let DATA = [...mockData];
+
+app.use('/address', (req, res, next) => {
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Express boilerplate initialized!');
+});
+
+app.get('/address', (req, res) => {
+  res.json(DATA);
+});
+
+app.post('/address', (req, res) => {
+  res.send('Express boilerplate initialized!');
+});
+
+app.delete('/address/:userId', (req, res) => {
+  const id = req.params.userId;
+
+  const validate = DATA.length;
+  DATA = DATA.filter(ad => ad.id !== parseInt(id));
+
+  if (DATA.length === validate) {
+    return res.status(400).send('Adress not found!');
+  }
+
+  res.json(DATA);
 });
 
 app.use(function errorHandler(error, req, res, next) {
---
+++ src/mockData.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Smith',
+    address1: 'here',
+    address2: 'there',
+    city: 'somewhere',
+    state: 'az',
+    zip: 12345
+  },
+  {
+    id: 2,
+    firstName: 'Jane',
+    lastName: 'Austin',
+    address1: 'somewhere',
+    address2: 'wherever',
+    city: 'again',
+    state: 'az',
+    zip: 12535
+  },
+  {
+    id: 3,
+    firstName: 'George',
+    lastName: 'Williams',
+    address1: 'here',
+    address2: 'there',
+    city: 'somewhere',
+    state: 'az',
+    zip: 10945
+  }
+];
